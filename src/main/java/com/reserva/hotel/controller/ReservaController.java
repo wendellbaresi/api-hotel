a27fdbc74d551f582a71184135aba69861c89f0d
Wendell Baresi <101334168+wendellbaresi@users.noreply.github.com>
2023-01-16 12:03:08 -0300

    Update ReservaController.java

--- src/main/java/com/reserva/hotel/controller/ReservaController.java
+++ src/main/java/com/reserva/hotel/controller/ReservaController.java
@@ -43,4 +43,57 @@ public class ReservaController {
         reserva.setEntrada(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.status(HttpStatus.CREATED).body(reservaService.salvar(reserva));
     }
+    
+    // Metodo de listar Reserva Unica pelo Id
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> UnicaReserva(@PathVariable(value = "id") Integer id){
+        // Criando um optional para buscar pelo id que foi criado no Service
+        Optional<Reserva> reservaOptional = reservaService.buscarPeloId(id);
+
+        // If criado para caso esteja vazio a vaga, ele informar que não foi encontrado a reserva
+        if (reservaOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Reserva não encontrada!");
+        // e se for encontrado ele vai pegar(get) a reserva
+        }else {
+            return ResponseEntity.status(HttpStatus.OK).body(reservaOptional.get());
+        }
+    }
+
+
+    //Metodo de Deletar Reserva pelo Id
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deletarReserva(@PathVariable(value = "id") Integer id){
+        Optional<Reserva> reservaOptional = reservaService.buscarPeloId(id);
+
+        // If criado para caso esteja vazio a vaga, ele informar que não foi encontrado a reserva
+        if (reservaOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Reserva não encontrada!");
+            // e se for encontrado ele vai deletar pelo id, e informar ao usuario
+        }else {
+            reservaService.deletarPeloId(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Reserva Deletada do banco de dados.");
+        }
+    }
+
+
+    // Metodo de Alterar a Reserva
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Object> alterarReserva(@PathVariable(value = "id") Integer id , @RequestBody @Valid ReservaDto reservaDto){
+        Optional<Reserva> reservaOptional = reservaService.buscarPeloId(id);
+
+        // If criado para caso esteja vazio a vaga, ele informar que não foi encontrado a reserva
+        if (reservaOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Reserva não Encontrada!");
+            // e se for encontrado ele vai fazer as alterações, pelos campos informados(entrada,saida,hospede,quarto)
+        }else {
+            var reserva = new Reserva();
+            reserva.setEntrada(reserva.getEntrada());
+            reserva.setSaida(reserva.getSaida());
+            reserva.setHospede(reserva.getHospede());
+            reserva.setQuarto(reserva.getQuarto());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(reservaService.salvar(reserva));
+        }
+    }
+    
 }
