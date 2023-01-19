f63c0ecb66ec2e0ae5de4a456ebe35b48c608e16
Wendell Baresi <101334168+wendellbaresi@users.noreply.github.com>
2023-01-18 22:29:22 -0300

    Update ReservaRepository.java

--- src/main/java/com/reserva/hotel/repository/ReservaRepository.java
+++ src/main/java/com/reserva/hotel/repository/ReservaRepository.java
@@ -1,15 +1,19 @@
 package com.reserva.hotel.repository;
 
+import com.reserva.hotel.model.Quarto;
 import com.reserva.hotel.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
-    public boolean existsByQuarto(String n_quarto);
+    @Query(value = "select c from Reserva c where (:quarto_id is null or c.quarto = :quarto_id) and (:entrada is null or c.entrada = :entrada)")
+    public List<Reserva> existsByQuartoIdAndEntrada(Quarto quarto_id, LocalDateTime entrada);
     public boolean existsByEntrada(LocalDateTime entrada);
 }
