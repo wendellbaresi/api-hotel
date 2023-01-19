edfef2b58a6a6105eddc78f07f733175154120ac
Wendell Baresi <101334168+wendellbaresi@users.noreply.github.com>
2023-01-18 22:28:45 -0300

    Update ReservaController.java

--- src/main/java/com/reserva/hotel/controller/ReservaController.java
+++ src/main/java/com/reserva/hotel/controller/ReservaController.java
@@ -10,9 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -22,28 +21,33 @@ public class ReservaController {
     @Autowired
     private ReservaService reservaService;
 
+
+
+    // Metodo de Listar todas as Reservas
     @GetMapping
     public List<Reserva> todasReservas(){
         return reservaService.reservas();
     }
 
+
+    // Metodo de Salvar Reservas
     @PostMapping
     public ResponseEntity<Object> salvarReservas( @RequestBody @Valid ReservaDto reservaDto){
-        if (reservaService.existsByQuarto(reservaDto.getQuarto())){
+        if (reservaService.existsByIdAndEntrada(reservaDto.getQuarto(), reservaDto.getEntrada())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Quarto reservado");
         }
-        if (reservaService.existsByEntrada(reservaDto.getEntrada())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Está data o quarto já foi reservado");
-        }
+
 
 
         var reserva = new Reserva();
         BeanUtils.copyProperties(reservaDto, reserva);
         // ENVIAR A DATA ATUAL DE ENTRADA DO HOSPEDE(Exemplo: Caso o hospede entre mais cedo ou mais tarde de sua resreva)
-        reserva.setEntrada(LocalDateTime.now(ZoneId.of("UTC")));
+//        reserva.setEntrada(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.status(HttpStatus.CREATED).body(reservaService.salvar(reserva));
     }
-    
+
+
+
     // Metodo de listar Reserva Unica pelo Id
     @GetMapping(value = "/{id}")
     public ResponseEntity<Object> UnicaReserva(@PathVariable(value = "id") Integer id){
@@ -95,5 +99,5 @@ public class ReservaController {
             return ResponseEntity.status(HttpStatus.CREATED).body(reservaService.salvar(reserva));
         }
     }
-    
+
 }
